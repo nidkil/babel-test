ed3fb42730e26a5d44d27f574671e46b4627148c
nidkil <info@nidkil.com>
2019-04-25 23:30:09 +0200

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _test = _interopRequireDefault(require("./test"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _test["default"])('Babel is working!');
+//# sourceMappingURL=index.js.map
---
+++ dist/test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var test = function test(msg) {
+  console.log(msg);
+};
+
+var _default = test;
+exports["default"] = _default;
+//# sourceMappingURL=test.js.map
